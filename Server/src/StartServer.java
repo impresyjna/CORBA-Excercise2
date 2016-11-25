6c321f381ed16092e59d7d126afab32d3a38b0fb
wampir1408 <joanna.furmaniak@student.put.poznan.pl>
2016-11-25 08:48:34 +0100

    Ready server

--- Server/src/StartServer.java
+++ Server/src/StartServer.java
@@ -1,5 +1,50 @@
-/**
- * Created by impresyjna on 25.11.2016.
- */
+
+import interfaces.UserService;
+import interfaces.UserServiceHelper;
+import org.omg.CosNaming.*;
+import org.omg.CosNaming.NamingContextPackage.*;
+import org.omg.CORBA.*;
+import org.omg.PortableServer.*;
+import org.omg.PortableServer.POA;
+import java.util.Properties;
+
 public class StartServer {
+
+    public static void main(String args[]) {
+        try{
+            // create and initialize the ORB //// get reference to rootpoa & activate the POAManager
+            ORB orb = ORB.init(args, null);
+            POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+            rootpoa.the_POAManager().activate();
+
+            // create servant and register it with the ORB
+            UserServiceObj addobj = new UserServiceObj();
+            addobj.setORB(orb);
+
+            // get object reference from the servant
+            org.omg.CORBA.Object ref = rootpoa.servant_to_reference(addobj);
+            UserService href = UserServiceHelper.narrow(ref);
+
+            org.omg.CORBA.Object objRef =  orb.resolve_initial_references("NameService");
+            NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+
+            NameComponent path[] = ncRef.to_name( "ABC" );
+            ncRef.rebind(path, href);
+
+            System.out.println("Server ready ...");
+
+            // wait for invocations from clients
+            for (;;){
+                orb.run();
+            }
+        }
+
+        catch (Exception e) {
+            System.err.println("ERROR: " + e);
+            e.printStackTrace(System.out);
+        }
+
+        System.out.println("HelloServer Exiting ...");
+
+    }
 }
